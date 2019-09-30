98a8101094f2833ac1012b1c1f6476623d12bfd0
Utkarsh Singh <55028178+utkarsh0407@users.noreply.github.com>
2019-10-01 02:14:34 +0530

    addition of two no.

---
+++ Task3/go/adding-two-no.go
@@ -0,0 +1,14 @@
+// Sum of n natural number (the positive numbers 1, 2, 3... are known as natural numbers)
+package main
+ 
+import "fmt"
+ 
+func main(){
+    var n,sum int    
+    fmt.Print("Enter a positive integer : ")
+    fmt.Scan(&n)
+    for i:=1; i<=n; i++ { // assigning 1 to i
+        sum += i // sum = sum + i
+    }
+    fmt.Print("Sum : ",sum)
+}
