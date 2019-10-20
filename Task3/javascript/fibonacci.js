48a09f3623599112384457f499867fc94e9149cd
Adrienne Lim <adriennelim.mc@gmail.com>
2019-10-19 23:55:58 -0700

    Add fibonacci memo approach.

--- Task3/javascript/fibonacci.js
+++ Task3/javascript/fibonacci.js
@@ -11,7 +11,7 @@
 
 export function fibonacci(end) {
   // If the number is not an integer, it's not part of the sequence. So return -1
-  if(!Number.isInteger(end)) {
+  if(!Number.isInteger(end) || end < 0) {
     return -1;
   } else {
     if(end > 1) {
@@ -24,10 +24,46 @@ export function fibonacci(end) {
     }
   }
 }
+
+/**
+ * An alternative solution using an array to store previously calculated fib numbers
+ * E.g.
+ *    end: 0,1,2,3,4,5,6
+ *    fib: 0,1,1,2,3,5,8
+ */
+export function fibonacciMemo(end) {
+  // Return -1 for invalid input
+  if (!Number.isInteger(end) || end < 0) {
+    return -1;
+  } 
+
+  if (end === 0 || end === 1) {
+    return end;
+  }
+
+  let memo = [0,1];
+  let index = 1;
+  
+  while (index <= end) {
+    index++;
+    memo[index] = memo[index - 1] + memo[index - 2];
+  }
+  
+  return memo[end];
+}
+
 export function fibTests() {
   console.assert(fibonacci(3.6) === -1); //not an integer
   console.assert(fibonacci(0) === 0); //base case
   console.assert(fibonacci(1) === 1); //base case
   console.assert(fibonacci(2) === 1); //example above
   console.assert(fibonacci(6) === 8); //another case for validation of normal path
+}
+
+export function fibMemoTests() {
+  console.assert(fibonacciMemo(3.6) === -1); //not an integer
+  console.assert(fibonacciMemo(0) === 0); //base case
+  console.assert(fibonacciMemo(1) === 1); //base case
+  console.assert(fibonacciMemo(2) === 1); //example above
+  console.assert(fibonacciMemo(6) === 8); //another case for validation of normal path
 }
