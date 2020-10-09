9e24b733d35e5e383365f0e07a2cc12e92782eb9
Jennifer Rosa <rosajen27@gmail.com>
2020-10-09 16:22:30 -0400

    task-3-rosajen27

---
+++ Task3/JavaScript/arithmetic.js
@@ -0,0 +1,18 @@
+// add, subtract, multiply, or divide two numbers together
+
+var add = function(num1, num2) {
+    return num1 + num2;
+  };
+  
+  var subtract = function(num1, num2) {
+    return num1 - num2;
+  };
+  
+  var multiply = function(num1, num2) {
+    return num1 * num2;
+  };
+  
+  var divide = function(num1, num2) {
+    return num1 / num2;
+  };
+  
